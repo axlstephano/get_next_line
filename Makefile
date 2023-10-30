60fe236c7ca3f5980f1524f78e24f2523c65aca7
stephanocastillo <axcastil@c2r13s6.42madrid.com>
2023-10-30 11:17:21 +0100

    301023

---
+++ Makefile
@@ -0,0 +1,29 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    Makefile                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: axcastil <axcastil@student.42.fr>          +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2023/10/29 23:16:42 by axcastil          #+#    #+#              #
+#    Updated: 2023/10/29 23:27:51 by axcastil         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+SRCS 		=	get_next_line.c get_next_line_utils.c
+
+CC 			=	gcc
+RM			=	rm -f
+CFLAGS		=	-Wall -Wextra -Werror
+NAME		= 	get_next_line
+all:		$(NAME)
+
+$(NAME):	$(SRCS)
+			@$(CC) $(SRCS) -o test
+
+cc:
+			@./test
+
+clean:		
+			@$(RM) test
+	
--- get_next_line.h
+++ get_next_line.h
@@ -1,9 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: axcastil <axcastil@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/30 00:33:57 by axcastil          #+#    #+#             */
+/*   Updated: 2023/10/30 10:48:08 by axcastil         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+
+# ifndef BUFFERSIZE
+#  define BUFFERSIZE 3
+# endif
 
 # include <unistd.h>
 # include <string.h>
 # include <stdlib.h>
 # include <stdio.h>
+# include <fcntl.h>
+
+char	*get_next_line(int fd);
+char	*ft_strjoin(char const *s1, char const *s2);
+size_t	ft_strlen(const char *str);
+char	*ft_strchr(const char *s, int c);
+char	*ft_substr(const char *s, unsigned int start, size_t len);
+char	*emptystr(char *str);
 
 #endif
