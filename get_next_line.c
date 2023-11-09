93b963f1bbaedbe119d4473ffcd62c9457763e1d
stephanocastillo <iamaxlstephano@gmail.com>
2023-11-09 20:25:54 +0100

    091023

--- get_next_line.c
+++ get_next_line.c
@@ -6,47 +6,69 @@
 /*   By: axcastil <axcastil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 16:40:53 by axcastil          #+#    #+#             */
-/*   Updated: 2023/11/08 20:53:39 by axcastil         ###   ########.fr       */
+/*   Updated: 2023/11/09 20:25:26 by axcastil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+char	*liner(char *buffer)
+{
+	char	*newline;
+	size_t	len;
+
+	len = 0;
+	while (!buffer[len] && buffer[len] != '\n')
+		len++;
+	newline = (char *)ft_calloc((len + 2), sizeof(char));
+	len = 0;
+	while (!buffer && buffer[len] != '\n')
+	{
+		newline[len] = buffer[len];
+		len++;
+	}
+	if (buffer[len] == '\n')
+		newline[len] = '\n';
+	return (newline);
+}
 
 char	*reader(int fd, char *result)
 {
 	char	*buffer;
 	size_t	byte_read;
 
-	if(!result)
+	if (!result)
 		result = ft_calloc(1, 1);
 	buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
 	byte_read = 1;
-	while(byte_read > 0)
+	while (byte_read > 0)
 	{
 		byte_read = read(fd, buffer, BUFFER_SIZE + 1);
 		if (byte_read == -1)
 			return (NULL);
 		buffer[byte_read] = '\0';
 		result = joinline(result, buffer);
-		if (ft_strchr(buffer, '\n'));
-			break;
+		if (ft_strchr(buffer, '\n'))
+			break ;
 	}
 	free(buffer);
 	return (result);
 }
 
-get_next_line(int fd)
+char	*get_next_line(int fd)
 {
 	static char	*result;
-	
-	//error handling
+	char		*line;
+
 	if (fd < 0 || read(fd, 0, 0) < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
 	result = reader(fd, result);
-	
+	if (!result)
+		return (NULL);
+	line = liner(result);
+	return (line);
 }
 
-//main
 int main(void)
 {
 	int		fd;
