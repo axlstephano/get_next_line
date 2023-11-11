1966e55f89f6686268ab573902f37500e6a0e074
stephanocastillo <iamaxlstephano@gmail.com>
2023-11-11 19:22:34 +0100

    1110232

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: axcastil <axcastil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 16:40:53 by axcastil          #+#    #+#             */
-/*   Updated: 2023/11/11 19:03:49 by axcastil         ###   ########.fr       */
+/*   Updated: 2023/11/11 19:13:20 by axcastil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,11 @@ char	*joinandfree(char *buffer1, char *buffer2)
 	new = ft_strjoin(buffer1, buffer2);
 	free(buffer1);
 	return(new);
+}
+
+char	*next_line(char	*buffer)
+{
+	
 }
 
 char	*liner(char *buffer)
@@ -67,16 +72,16 @@ char	*reader(int fd, char *result)
 char	*get_next_line(int fd)
 {
 	static char	*result;
-	char		*line;
+	char		*new_line;
 
 	if (fd < 0 || read(fd, 0, 0) < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
 	result = reader(fd, result);
 	if (!result)
 		return (NULL);
-	//line = liner(result);
-	//buffer = nextline(buffer);
-	return (line);
+	new_line = liner(result);
+	result = next_line(result);
+	return (new_line);
 }
 
 int main(void)
@@ -85,6 +90,8 @@ int main(void)
 	char	*parson;
 
 	fd = open("text.txt", O_RDONLY);
+	parson = get_next_line(fd);
+	printf("%s", parson);
 	parson = get_next_line(fd);
 	printf("%s", parson);
 	close(fd);
