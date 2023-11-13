9a24b255f108afe5949b8c46d402d3838ef98869
stephanocastillo <iamaxlstephano@gmail.com>
2023-11-13 18:07:05 +0100

    stable + paco

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -6,7 +6,7 @@
 /*   By: axcastil <axcastil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/12 20:20:54 by axcastil          #+#    #+#             */
-/*   Updated: 2023/11/13 17:51:42 by axcastil         ###   ########.fr       */
+/*   Updated: 2023/11/13 18:06:43 by axcastil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -109,3 +109,36 @@ char	*get_next_line(int fd)
 	result[fd] = next_line(result[fd]);
 	return (new_line);
 }
+//int main()
+//{
+//    int fd1;
+//	int	fd2;
+//	int	fd3;
+//    char *result1;
+//	char *result2;
+//	char *result3;
+
+//    // Archivo 1: text.txt
+//    fd1 = open("text1.txt", O_RDONLY);
+//	fd2 = open("text2.txt", O_RDONLY);
+//	fd3 = open("text3.txt", O_RDONLY);
+//    if (fd1 == -1 || fd2 == -1 || fd3 == -1)
+//        return 0;
+//    result1 = get_next_line(fd1);
+//	result2 = get_next_line(fd2);
+//    result3 = get_next_line(fd3);
+//	while (result1 || result2 || result3)
+//    {
+//		printf("%s%s%s\n", result1, result2, result3);
+//        free(result1);
+//		free(result2);
+//		free(result3);
+//		result1 = get_next_line(fd1);
+//		result2 = get_next_line(fd2);
+//		result3 = get_next_line(fd3);
+//    }
+//    close(fd1);
+//	close(fd2);
+//	close(fd3);
+//    return 0;
+//}
